9db3932415b19a8f8a8cefdfa69d5dd4fea26402
GuillBuj <guillaume.bujon@gmail.com>
2024-11-07 15:42:36 +0100

    Boucle for + Mots/Phrases

--- script.js
+++ script.js
@@ -1,45 +1,52 @@
-/*const motApplication = "Bonjour"
-let motUtilisateur = prompt("Entrez le mot: " + motApplication)
-if(motUtilisateur === motApplication){
-    console.log("Bravo!")    
-} else {
-    console.log("Nul!")
-}*/
-
 let listeMots = ["Cachalot", "Pétunia", "Serviette"]
+let listePhrases = ["Pas de panique !", "La vie, l\’univers et le reste", "Merci pour le poisson"]
 
 let score = 0
-console.log(score)
 
-let motApplication = listeMots[0]
-let motUtilisateur = prompt("Saisissez le mot \"" + motApplication + "\"")
-if(motUtilisateur === motApplication){
-    score++
-    console.log("Bravo!")
-} else {
-    console.log("Nul!")
-}
-console.log(score + "/1")
+let choixUtilisateur = prompt("Choisissez:\n(1) Mots\n(2) Phrases")
+while(choixUtilisateur !== "Mots" && choixUtilisateur !== "Phrases2"&&
+    choixUtilisateur !== "mots" && choixUtilisateur !== "phrases"&&
+    choixUtilisateur !== "1" && choixUtilisateur !== "2"
+) {choixUtilisateur = prompt("Choisissez:\n(1) Mots\n(2) Phrases")}
 
-motApplication = listeMots[1]
-motUtilisateur = prompt("Saisissez le mot \"" + motApplication + "\"")
-if(motUtilisateur === motApplication){
-    score++
-    console.log("Bravo!")
-} else {
-    console.log("Nul!")
-}
-console.log(score + "/2")
 
-motApplication = listeMots[2]
-motUtilisateur = prompt("Saisissez le mot \"" + motApplication + "\"")
-if(motUtilisateur === motApplication){
-    score++
-    console.log("Bravo!")
-} else {
-    console.log("Nul!")
+console.log(choixUtilisateur)
+switch(choixUtilisateur){
+    case "1":
+    case "Mots":
+    case "mots":
+        for(let i=0; i<listeMots.length; i++){
+            let motApplication = listeMots[i]
+            let motUtilisateur = prompt("Saisissez le mot \"" + motApplication + "\"")
+            if(motUtilisateur === motApplication){
+                score++
+                console.log("Bravo!")
+            } else {
+                console.log("Nul...")
+            }
+            n=i+1
+            console.log(score + "/" + n)
+        }
+    case "2":
+    case "Phrases":
+    case "phrases":
+        for(let i=0; i<listePhrases.length; i++){
+            let phraseApplication = listePhrases[i]
+            let phraseUtilisateur = prompt("Saisissez le mot \"" + phraseApplication + "\"")
+            if(phraseUtilisateur === phraseApplication){
+                score++
+                console.log("Bravo!")
+            } else {
+                console.log("Nul...")
+            }
+            n=i+1
+            console.log(score + "/" + n)
+        }
+    default:
+        break;
 }
-console.log(score + "/3")
+
+
 
 
 

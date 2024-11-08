d04b32c8c9cdaaf6b32f3a668d5e417e28422c8c
GuillaumeBuj <guillaume.bujon@gmail.com>
2024-11-08 19:33:42 +0100

    Recuperation d'elements dans le main

---
+++ V2/scripts/main.js
@@ -0,0 +1,22 @@
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
+
+lancerJeu()
+
+let inputEcriture = document.getElementById("inputEcriture")
+console.log(inputEcriture)
+
+let btnValiderMot = document.getElementById("btnValiderMot")
+console.log(inputEcriture)
+
+let zoneProposition = document.querySelector(".zoneProposition")
+console.log(zoneProposition)
+
+let zoneScore = document.querySelector(".zoneScore span")
+console.log(zoneScore)
+
+let listeBtnRadio = document.querySelectorAll(".optionSource input")
+console.log(listeBtnRadio)
